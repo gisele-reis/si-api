992379191d2ea8af4ac479f6a3138bbe18819978
Gisele Reis <giselealvesdosreis@gmail.com>
2024-10-01 22:43:37 -0300

    :sparkles: feat: add novos campos de usuario

--- src/users/create-user.dto.ts
+++ src/users/create-user.dto.ts
@@ -1,4 +1,23 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsNumber({ allowInfinity: false, allowNaN: false, maxDecimalPlaces: 2 })
+  peso: number;
+
+  @IsNotEmpty()
+  @IsNumber({ allowInfinity: false, allowNaN: false, maxDecimalPlaces: 2 })
+  altura: number;
+
+  @IsNotEmpty()
+  termos: boolean;
 }
--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -2,12 +2,24 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
   @Column()
   username: string;
 
   @Column()
   password: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  peso: number;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  altura: number;
+
+  @Column()
+  termos: boolean;
 }
